c25b1a9623202eab1cb2a3addafaba49338d93d3
Kamal Uddin Panhwar <kamalpanhwar@gmail.com>
2021-04-21 12:22:17 +0500

    Added feature test case for links on the pages

--- spec/features/scores_spec.rb
+++ spec/features/scores_spec.rb
@@ -4,10 +4,12 @@ describe 'The home page with score table', type: :feature do
   scenario 'user can see index score board' do
     visit '/scores'
     expect(page).to have_content(I18n.t('scores.index.heading'))
+    expect(page).to have_content(I18n.t('scores.index.link_to_actual'))
   end
 
   scenario 'user can see actual file' do
     visit '/actual_file'
     expect(page).to have_content(I18n.t('scores.actual_file.heading'))
+    expect(page).to have_content(I18n.t('scores.actual_file.back'))
   end
 end
